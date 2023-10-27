0a313bed2e90e18b5f18a2284ec6ed5a99d7dc9b
zorochimaru777 <rasim.karimli@gmail.com>
2023-10-27 20:27:48 +0400

    Vote app - github - Update vite envs

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASECONFIG_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASECONFIG_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASECONFIG_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASECONFIG_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASECONFIG_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASECONFIG_APP_ID
+  apiKey: import.meta.env.VITE_INTERNAL_FIREBASECONFIG_API_KEY,
+  authDomain: import.meta.env.VITE_INTERNAL_FIREBASECONFIG_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_INTERNAL_FIREBASECONFIG_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_INTERNAL_FIREBASECONFIG_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_INTERNAL_FIREBASECONFIG_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_INTERNAL_FIREBASECONFIG_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
